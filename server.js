ab8aa1533df64b29543829542dee5364be97310f
hostasite <30488142+hostasite@users.noreply.github.com>
2017-10-30 11:32:14 +1100

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ var MBTiles = require('@mapbox/mbtiles');
 var app = express();
 var LRU = require("lru-cache")
   , options = { max: 100000
-			  , length: function (key, n) { return fs.statSync(key)['size']/1000000.0}
-			  , dispose: function (key, n) { console.log('dispose',n,fs.statSync(n)); fs.statSync(n) && fs.unlink(n);  }
-              , maxAge: 1000 * 60 * 60} // 1 Hour
+  , length: function (key, n) { return fs.statSync(key)['size']/1000000.0}
+  , dispose: function (key, n) { console.log('dispose',n,fs.statSync(n)); fs.statSync(n) && fs.unlink(n);  }
+  , maxAge: 1000 * 60 * 60} // 1 Hour
   , cache = LRU(options);
-
 var AWS = require('aws-sdk');
 if (process.argv.length < 4) {
   console.log("Error! Missing AWS Credentials.\n accessKeyId & secretAccessKey required");
   process.exit(1);
 }
+// FETCH AWS CREDENTIALS FROM args
 accessKeyId = process.argv[2];
 secretAccessKey = process.argv[3];
 AWS.config.update({
@@ -23,7 +23,6 @@ AWS.config.update({
 	region: "ap-southeast-2"
 	});
 var s3 = new AWS.S3();
-
 var port = 3000;
 
 app.get('/:z/:x/:y.*', function(req, res){
@@ -32,7 +31,7 @@ app.get('/:z/:x/:y.*', function(req, res){
 		var zeroString = Math.pow(10, zeros).toString().substr(1);
 		return zeroString + num;
 	}
-	// CREATE BUNDLE FILE NAMES USING Z, X, Y
+	// CREATE MBTILE FILE NAMES USING Z, X, Y
 	var level = req.param('z');
 	var col = req.param('x');
 	var row = req.param('y');
@@ -41,11 +40,10 @@ app.get('/:z/:x/:y.*', function(req, res){
 	var colName = zeroPad(colVal.toString(16), 4);
 	var rowName = zeroPad(rowVal.toString(16), 4);
 	var levelName = zeroPad(level.toString(10), 2);
-	
 	mbtfile = '/R' + rowName + 'C' + colName;
-	var temp = '/home/ubuntu/mbtcache/L' + level; // TEMP FOLDER FOR CACHE IN EC2
+	var temp = '/home/ubuntu/mbtcache/L' + level; // TEMP FOLDER CREATED FOR CACHE IN EC2
 	var tempFile = temp + mbtfile + '.mbtiles';
-	if ((fs.existsSync(tempFile)) && (fs.statSync(tempFile)['size'] != 0)){
+		if ((fs.existsSync(tempFile)) && (fs.statSync(tempFile)['size'] != 0)){
 		var mbt = cache.get(mbtfile);
 		if(mbt){
 			cache.get(mbtfile);
